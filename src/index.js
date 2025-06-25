45cb314ede13f620779510fccb21b5f8559b0a38
night2408 <nishitasolanki588@gmail.com>
2025-06-25 11:49:02 +0400

    Initial commit or your message

--- src/index.js
+++ src/index.js
@@ -1,31 +1,53 @@
-import { nanoid } from "nanoid";
-import moduleName from './style.css'
-
 const urlParams = new URLSearchParams(location.search);
 
-let roomId=urlParams.get("id");
-if(!roomId){
-    roomId= Math.floor(Math.random()*10000+10000);
-    window.location.search =`id=${roomId}`;
+let roomId = urlParams.get("id");
+if (!roomId) {
+  roomId = Math.floor(Math.random() * 10000 + 10000);
+  window.location.search = `id=${roomId}`;
 }
 
-const textArea = document.querySelector("textarea");
-
+// Setup WebSocket
 const wsurl = `wss://s14842.blr1.piesocket.com/v3/${roomId}?api_key=6uOwcOJZ3ZMuoixiljKa01NXOhrSoT5QqWnwW8PB&notify_self=1`;
+const socket = new WebSocket(wsurl);
 
-const socket= new WebSocket(wsurl);
+// Elements
+const textArea = document.getElementById("editor");
+const typingStatus = document.getElementById("typingStatus");
+const roomLinkInput = document.getElementById("roomLink");
+const copyBtn = document.getElementById("copyBtn");
 
-socket.onopen=() =>{};
+// Set room link
+roomLinkInput.value = window.location.href;
 
+// Copy button logic
+copyBtn.addEventListener("click", () => {
+  roomLinkInput.select();
+  document.execCommand("copy");
+  copyBtn.innerText = "Copied!";
+  setTimeout(() => (copyBtn.innerText = "📋 Copy Link"), 2000);
+});
+
+// WebSocket events
+socket.onopen = () => {};
 
 socket.onmessage = (e) => {
-    //console.log(e.data);
+  if (e.data === "__typing__") {
+    typingStatus.innerText = "Your buddy is typing...";
+    clearTimeout(typingStatus.timeout);
+    typingStatus.timeout = setTimeout(() => {
+      typingStatus.innerText = "";
+    }, 1000);
+  } else {
     textArea.value = e.data;
+  }
 };
 
-
-textArea.addEventListener("change",(e)=>{
-    //console.log(e.target.value);
-    socket.send(e.target.value);
-})
-
+// Typing event
+let typingTimer;
+textArea.addEventListener("input", () => {
+  socket.send("__typing__");
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(() => {
+    socket.send(textArea.value);
+  }, 300);
+});
